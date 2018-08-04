e7bb7c9c4db6273bcf47aca663b1e0bb8fa2234d
pokered <brahman98@gmail.com>
2018-08-04 22:16:36 +0900

    start

---
+++ PokemonUtility/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Prism.Mvvm;
+using Prism.Commands;
+using PokemonUtility.Models;
+
+namespace PokemonUtility.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "Prism Application";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        private MainModel model;
+
+        public MainWindowViewModel()
+        {
+            model = new MainModel();
+        }
+
+        private DelegateCommand calcComamnd;
+        public DelegateCommand calcCommand
+        {
+            get { return calcComamnd = calcComamnd ?? new DelegateCommand(CHangeTitle); }
+        }
+
+        private void CHangeTitle()
+        {
+            Title = model.CHangeTitle();
+        }
+    }
+}
---
+++ PokemonUtility/Views/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using PokemonUtility.ViewModels;
+
+namespace PokemonUtility.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = new MainWindowViewModel();
+        }
+    }
+}
