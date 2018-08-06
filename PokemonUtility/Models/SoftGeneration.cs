ae1b04c7deb6688bd1887f9e09393108e4587f0b
pokered <brahman98@gmail.com>
2018-08-06 20:45:22 +0900

    aa

---
+++ PokemonUtility/Models/SoftGeneration.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonUtility.Models
+{
+    class SoftGeneration
+    {
+        /// <summary>
+        /// IDを取得または設定します。
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// ソフトの世代名を取得または設定します。
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
