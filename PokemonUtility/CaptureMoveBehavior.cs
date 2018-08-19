bb6421d412a308efb32a186a4372ee0e29fab9e1
pokered <brahman98@gmail.com>
2018-08-19 20:24:24 +0900

    aa

---
+++ PokemonUtility/CaptureMoveBehavior.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Interactivity;
+
+namespace PokemonUtility
+{
+    class CaptureMoveBehavior : Behavior<Button>
+    {
+        protected override void OnAttached()
+        {
+            // AssociatedObjectのイベントを購読する
+            this.AssociatedObject.Click += this.ButtonClicked;
+        }
+
+        protected override void OnDetaching()
+        {
+            // イベントの購読解除
+            this.AssociatedObject.Click += this.ButtonClicked;
+        }
+
+        // イベントで処理をする
+        private void ButtonClicked(object sender, System.Windows.RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello world");
+        }
+    }
+
+    class CaptureMoveBehavior2 : Behavior<Border>
+    {
+        protected override void OnAttached()
+        {
+            // AssociatedObjectのイベントを購読する
+            this.AssociatedObject.MouseDown += this.ButtonClicked;
+        }
+
+        // イベントで処理をする
+        private void ButtonClicked(object sender, System.Windows.RoutedEventArgs e)
+        {
+            MessageBox.Show("Hello world");
+        }
+    }
+}
