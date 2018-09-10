64286c60e95064cc5efc5831c46f43dcd831ecf4
サム <brahman98@gmail.com>
2018-09-10 15:43:23 +0900

    aa

--- PokemonUtility/TriggerActions/CaptureWindowAction.cs
+++ PokemonUtility/TriggerActions/CaptureWindowAction.cs
@@ -24,21 +24,28 @@ namespace PokemonUtility.TriggerActions
             CaptureWindowViewModel vm = (CaptureWindowViewModel)window.DataContext;
 
             FitWithinScreen(ref window, ref rec);
-            
-            window.Left = rec.X;
-            window.Top = rec.Y;
-            window.Width = rec.Width;
-            window.Height = rec.Height;
+
+            if (window.WindowStartupLocation == System.Windows.WindowStartupLocation.Manual)
+            {
+                window.Left = rec.X;
+                window.Top = rec.Y;
+                window.Width = rec.Width;
+                window.Height = rec.Height;
+            }
         }
 
         protected override void ApplyWindowToNotification(Window windown, INotification notification)
         {
             RectangleNotification rec = ((RectangleNotification)notification);
             CaptureWindowViewModel vm = (CaptureWindowViewModel)windown.DataContext;
-            rec.X = vm.X;
-            rec.Y = vm.Y;
-            rec.Width = vm.Width;
-            rec.Height = vm.Height;
+
+            if (Window.WindowStartupLocation == System.Windows.WindowStartupLocation.Manual)
+            {
+                rec.X = vm.X;
+                rec.Y = vm.Y;
+                rec.Width = vm.Width;
+                rec.Height = vm.Height;
+            }
         }
 
         // ウィンドウをスクリーン内に収める
@@ -49,21 +56,23 @@ namespace PokemonUtility.TriggerActions
             screenRect.Width = (int)SystemParameters.PrimaryScreenWidth;
             screenRect.Height = (int)SystemParameters.PrimaryScreenHeight;
 
-            // キャプチャ範囲の最小範囲
-            if (CaptureRect.Width < 50 || CaptureRect.Height < 50)
-            {
-                CaptureRect.Width = (int)(screenRect.Width / 2);
-                CaptureRect.Height = (int)(screenRect.Height / 2);
-            }
-
+            // キャプチャ範囲がスクリーン外の場合中央に表示する
             Point captureLeftTop = new Point(CaptureRect.X, CaptureRect.Y);
             Point captureRightBottom = new Point(CaptureRect.X + CaptureRect.Width, CaptureRect.Y + CaptureRect.Height);
 
             if (!screenRect.Contains(captureLeftTop) || !screenRect.Contains(captureRightBottom))
             {
                 window.WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
-                CaptureRect.X = (int)window.Left;
-                CaptureRect.Y = (int)window.Top;
+            }
+
+            // キャプチャサイズが範囲外の場合
+            if (CaptureRect.Width < 50
+                || CaptureRect.Height < 50
+                || CaptureRect.Width > screenRect.Width
+                || CaptureRect.Height > screenRect.Height)
+            {
+                CaptureRect.Width = (int)(screenRect.Width / 2);
+                CaptureRect.Height = (int)(screenRect.Height / 2);
             }
         }
     }
