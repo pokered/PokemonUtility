34c26c702b85ad7f4c9e5495e5d7b04b023465b2
pokered <brahman98@gmail.com>
2018-09-14 02:02:49 +0900

    aa

---
+++ PokemonUtility/Models/PokemonImageModel.cs
@@ -0,0 +1,22 @@
+﻿using Prism.Mvvm;
+using System.Windows.Media.Imaging;
+
+namespace PokemonUtility.Models
+{
+    class PokemonImageModel : BindableBase
+    {
+        private int _pokemonID = -1;
+        public int PokemonID
+        {
+            get { return _pokemonID; }
+            set { SetProperty(ref _pokemonID, value); }
+        }
+
+        private BitmapImage bmp;
+        public BitmapImage Bmp
+        {
+            get { return bmp; }
+            set { SetProperty(ref bmp, value); }
+        }
+    }
+}
