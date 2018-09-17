aa0f6efb4a7d52d9ca3153f251bcf9163fa664db
pokered <brahman98@gmail.com>
2018-09-18 05:02:57 +0900

    aa

--- PokemonUtility/Models/PokemonImageModel.cs
+++ PokemonUtility/Models/PokemonImageModel.cs
@@ -1,4 +1,5 @@
 ﻿using Prism.Mvvm;
+using System;
 using System.Windows.Media.Imaging;
 
 namespace PokemonUtility.Models
@@ -9,7 +10,11 @@ namespace PokemonUtility.Models
         public int ID
         {
             get { return _id; }
-            set { SetProperty(ref _id, value); }
+            set
+            {
+                SetProperty(ref _id, value);
+                ChangeImage();
+            }
         }
 
         private BitmapImage _image;
@@ -17,6 +22,24 @@ namespace PokemonUtility.Models
         {
             get { return _image; }
             set { SetProperty(ref _image, value); }
+        }
+
+        private void ChangeImage()
+        {
+            // UIスレッドで行う例
+            var source = new BitmapImage();
+            BitmapImage img = new BitmapImage();
+            img.BeginInit();
+            img.UriSource = new Uri("C:\\Users\\pokered\\Source\\Repos\\PokemonUtility\\PokemonUtility\\Images\\pokemon\\0\\icon.png");
+            img.EndInit();
+
+            Image = img;
+        }
+
+        public PokemonImageModel()
+        {
+            Image = new BitmapImage();
+            ID = 0;
         }
     }
 }
